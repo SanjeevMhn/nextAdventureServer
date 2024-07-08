5bf78d3af4b79189f6a00004266de3cdc0aa373a
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-07-08 17:30:06 +0545

    INITIAL COMMIT

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+	res.status(200)
+		.json({
+			message: 'Hello World'
+		})
+})
+
+app.listen(port, () => {
+	console.log(`Server listening in port ${port}`);
+})
